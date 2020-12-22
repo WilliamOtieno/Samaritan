4f2c24dbc1206a5b14574b9cfe2fbce8bda911c0
William Otieno <jimmywilliamotieno@gmail.com>
2020-12-22 08:16:12 +0000

    added minimal boiler

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
+
+client.run(os.getenv('TOKEN'))
+
