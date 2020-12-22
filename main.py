2ce445dedd2d93f0c9f5e55ca6ce6fe328b869b1
William Otieno <jimmywilliamotieno@gmail.com>
2020-12-22 08:41:38 +0000

    added inspiration

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import discord
 import os
+import requests
+import json
+import random
 
 client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "grave", "hard", "lonely", "sorry", "upset", "troubled", "demise", "disappointed", "death", "obituary", "disaster", "funeral", "gloomy", "sombre", "dismal", "rejected", "gloomy", "unhappy", "miserable", "angry"]
+
+starter_encouragements = [
+    "Cheer up! ",
+    "Hang in there. ",
+    "You're a great person. ",
+    "Don't worry. "
+]
+
+
+def get_quote():
+    response = requests.get('https://zenquotes.io/api/random')
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q']
+    return quote
+
 
 @client.event
 async def on_ready():
@@ -12,8 +32,17 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('$hello'):
+    msg = message.content
+
+    if msg.startswith('.hello'):
         await message.channel.send('Hello!')
+
+    if msg.startswith('.inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+    if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
     
 
 client.run(os.getenv('TOKEN'))
